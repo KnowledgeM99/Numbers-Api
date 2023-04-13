af6478cdfcc7105ee17a7b8d78d00042e787c43c
Knowledge Mongoe <113101223+KnowledgeM99@users.noreply.github.com>
2023-04-13 14:46:07 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import NumbersApi from './Components/NumbersAPI';
+
+
+
+function App() {
+  return (
+    <div className="App">
+    
+     <NumbersApi/>
+    
+    </div>
+  );
+}
+
+export default App;
+
